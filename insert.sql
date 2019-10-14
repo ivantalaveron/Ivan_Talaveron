a93f8253697a64057992a456505ff3fcf6919290
ismael <ismaelpl9090@gmail.com>
2019-10-14 10:31:40 +0200

    aula

--- insert.sql
+++ insert.sql
@@ -4,3 +4,6 @@ VALUES (3, 'Jose Antonio', 'Galán', 21, 'C/La mua');
 
 INSERT INTO cursos (codigo_curso, nombre, Descripcion, Horas_duracion)
 VALUES (5, 'ASIR', 'Curso de Administracion Redes', 150);
+
+INSERT INTO aula (nombre, Capacidad, Planta, Descripción)
+VALUES ('aso',50, 1, 'buen estado';
