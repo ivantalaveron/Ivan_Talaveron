b63d2d177e64e76b2e78efc2f513c4875a394388
Jose Antonio <llytocolegio123@gmail.com>
2019-10-14 10:24:01 +0200

    Insertar cursos

--- insert.sql
+++ insert.sql
@@ -1,2 +1,6 @@
 INSERT INTO usuarios (id, nombre, Apellidos, Edad, Direccion)
 VALUES (3, 'Jose Antonio', 'Galán', 21, 'C/La mua');
+
+
+INSERT INTO cursos (codigo_curso, nombre, Descripcion, Horas_duracion)
+VALUES (5, 'ASIR', 'Curso de Administracion Redes', 150);
